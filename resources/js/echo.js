5521747b28192d11b1701397cd32bb6d4bf8faf5
Ayan <karmakarayan217@gmail.com>
2024-08-23 23:44:16 +0530

    vite env added

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,8 +4,9 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'ead6ef25805345a5caa8',
-    cluster: 'ap2',
-    forceTLS: true
-  });
+  broadcaster: 'pusher',
+  key: import.meta.env.VITE_PUSHER_APP_KEY,
+  cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+  forceTLS: true
+});
+
